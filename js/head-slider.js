414a9726f42180d47aef28d64466ec3f0aed78a3
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2022-09-25 15:07:41 +0500

    new-c

--- js/head-slider.js
+++ js/head-slider.js
@@ -6,3 +6,13 @@ flashSimpleSlider(".head-slider", {
 	speed: "2.5s",
 	draggable: false,
 });
+
+
+// slider-links
+
+const links = document.querySelectorAll('.head-slide-link');
+links.forEach(link => link.addEventListener('click', function(event) {
+	 event.stopPropagation(); 
+}));
+
+// slider-links
